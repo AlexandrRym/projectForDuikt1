bcf3aee9eb5ce17916caaad16f41aa333ac7be03
AlexandrRym <145048428+AlexandrRym@users.noreply.github.com>
2023-09-17 23:19:12 +0300

    Add files via upload

---
+++ AntArt/src/AntArt/AsciAnt.java
@@ -0,0 +1,20 @@
+package AntArt;
+
+public class AsciAnt {
+
+	public static void main(String[] args) {
+		System.out.println("    _.._.-..-._");
+		System.out.println(" .-'  .'  /\\  \\`._");
+		System.out.println("/    /  .'  `-.\\  `.");
+		System.out.println("    :_.'  ..    :       _.../\\");
+		System.out.println("    |           ;___ .-'   //\\\\.");
+		System.out.println("     \\  _..._  /    `/\\   //  \\\\\\");
+		System.out.println("      `-.___.-'  /\\ //\\\\       \\\\:");
+		System.out.println("           |    //\\V/ :\\\\       \\\\");
+		System.out.println("            \\      \\\\/  \\\\      /\\\\");
+		System.out.println("             `.____.\\\\   \\\\   .'  \\\\");
+		System.out.println("               //   /\\\\---\\\\-'     \\\\");
+		System.out.println("              //   // \\\\   \\\\       \\\\");
+	}
+
+}
---
+++ AntArt/src/AntArt/DisplayMyVisitingCard.java
@@ -0,0 +1,15 @@
+package AntArt;
+
+public class DisplayMyVisitingCard {
+
+	public static void main(String[] args) {
+		System.out.println("Римар Олександр Андрійович");
+		System.out.println("Student");
+		System.out.println("mob: +380-000-00-00");
+		System.out.println("email: emmeeil@gmail.com");
+		System.out.println("Solomyanska str.7,");
+		System.out.println("Ukraine, Kyiv, 06 999");
+		
+	}
+
+}
